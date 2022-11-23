bd2c7a201bcfd3eab358a7c51949ff0b5916489d
Muhammed Aslam <muhammmedaslamm312@gmail.com>
2022-11-23 09:52:24 +0530

    staticpushed

--- lms_api/settings.py
+++ lms_api/settings.py
@@ -161,9 +161,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/'static'
-STATICFILES_DIRS=[
-    'lms_api/static'
-]
+# STATICFILES_DIRS=os.path.join(BASE_DIR, 'static',)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, ''),)
 
 MEDIA_URL='/media/'
 MEDIA_ROOT= BASE_DIR/'media'
--- lms_api/urls.py
+++ lms_api/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include("payment.urls")),
      path('api/', include("cirtificate.urls")),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT,)
